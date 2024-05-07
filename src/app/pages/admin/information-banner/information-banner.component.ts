0f1cf684f6c3915c08d92a9e85df7d26b8c8cc35
Dai-Trinh <97729352+Dai-Trinh@users.noreply.github.com>
2024-05-07 23:51:48 +0700

    new banner

--- src/app/pages/admin/information-banner/information-banner.component.ts
+++ src/app/pages/admin/information-banner/information-banner.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MessageService } from '../../../services/message.service';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { NzResizeEvent } from 'ng-zorro-antd/resizable';
 
 @Component({
   selector: 'app-information-banner',
@@ -6,5 +10,133 @@ import { Component } from '@angular/core';
   styleUrl: './information-banner.component.css'
 })
 export class InformationBannerComponent {
+
+  constructor(
+    private _messageService: MessageService,
+    private _router: Router,
+    private _translateService: TranslateService
+  ) {
+    this._translateService
+      .get('notification.valid_action') // <- lấy theo key nào (Ex: 'notification.valid_action', ...)
+      .subscribe((item) => (this.validAction = item)); // <- lấy dữ liệu từ file JSON ngôn ngữ
+  }
+
+  title: string = "Danh sách Banner"
+  total: number = 10
+  validAction: string = ""
+  countSort = 0;
+  sortOrder: string = 'DESC';
+  sortProperty: string = 'updatedAt';
+  filter: any = {};
+
+  // TODO: selector app-pagination
+  page: number = 0;
+  perPage: number = 10;
+
+  lstData: any[] = [];
+
+
+  columns: any[] = [
+   
+    {
+      title: 'Ảnh banner',
+      key: 'imageBanner',
+      width: '150px',
+      visible: true,
+      sortOrder: '',
+    },
+    {
+      title: 'Ẩn/hiện',
+      key: 'visible',
+      width: '150px',
+      visible: true,
+      sortOrder: '',
+    },
+    {
+      title: 'Thời gian cập nhật',
+      key: 'updatedAt',
+      width: '150px',
+      visible: true,
+      sortOrder: '',
+    },
+    
+  ];
+
+  onHandlePagination(event: any) {}
+
+  handleClickButton(){
+
+  }
+
+  onHandleChangeColumn($event: any) {
+    this.columns = [...$event];
+    // localStorage.setItem('columnShipBunkering', JSON.stringify(this.columns));
+  }
+
+  id = -1;
+  onResize({ width }: NzResizeEvent, i: number): void {
+    cancelAnimationFrame(this.id);
+    this.id = requestAnimationFrame(() => {
+      this.columns[i].width = width + 'px';
+    });
+  }
+
+  handleSort(column: any, event: any) {
+    this.columns.forEach((element) => {
+      element.sortOrder = '';
+    });
+    column.sortOrder = event;
+    this.sortOrder = event;
+    this.sortProperty = column.key;
+  }
+
+  handleFilterFollowStatus(status: any) {
+    this.filter.status = status;
+    if (this.filter.status) {
+      // Gọi API
+    }
+  }
+
+  onHandleFilter(event: any, column: any) {
+    this.filter[column.key] = event;
+    console.log(event);
+  }
+
+  onHandleClearFilter(column: any) {
+    this.filter[column.key] = {
+      value: null,
+      operator: 0,
+    };
+    console.log(this.filter);
+    // Gọi API
+  }
+
+  // TODO  selectors: app-modal-content
+  visibleModal: boolean = false;
+  openModalConfirm() {
+    this.visibleModal = true;
+  }
+
+  handleConfirm() {
+    alert('Click confirm');
+  }
+
+  // TODO: selector: app-breadscrumb
+  breadcrumbs: any = [
+    {
+      name: 'Home',
+      route: ``,
+    },
+    {
+      name: 'Room',
+      route: ``,
+    },
+    {
+      name: 'Bed',
+      route: ``,
+    },
+  ];
+
+
 
 }
